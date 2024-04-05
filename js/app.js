fb64597575c2f050064442b2b27742655fe105ad
Amanda DiNoto <amanda.dinoto@gmail.com>
2024-04-05 15:33:10 -0400

    add math functionality to buttons to allow calculator to work properly

--- js/app.js
+++ js/app.js
@@ -6,25 +6,45 @@
   - Function to perform calcs / stack input
 
   - Button for light/dark mode
+  - Make sure to set character limit/number limit for display string.
+  - have a comma popup in appropriate places
 */ 
 
-//Buttons
+//Elements
 const numButtons = document.querySelectorAll('button.num');
 const opButtons = document.querySelectorAll('button.operation');
 const clear = document.getElementById('clear');
 const backspace = document.getElementById('backspace');
 const decimal = document.getElementById('decimal');
 const enter = document.getElementById('enter');
-
-//Other elements
 const display = document.getElementById('num-display');
 
+// Variables
+let firstNum = 0;
+let secondNum = 0;
+let total = 0;
+let isDivideActive = false;
+let isMultiplyActive = false;
+let isSubtractActive = false;
+let isAddActive = false;
 
+// Event listeners for buttons
 numButtons.forEach(function(button) {
   button.addEventListener('click', function() {
-    if (display.innerText === '0') display.innerText = '';
 
-    display.innerText = display.innerText + button.innerText;
+    if (
+      isDivideActive === true || 
+      isMultiplyActive === true || 
+      isSubtractActive === true || 
+      isAddActive === true
+    ) {
+      display.innerText = '0';
+      manageDisplay(button);
+      secondNum = display.innerText;
+    } else {
+      manageDisplay(button);
+      firstNum = display.innerText;
+    }
   });
 });
 
@@ -35,34 +55,44 @@ opButtons.forEach(function(button) {
     decimal.removeAttribute('disabled');
 
     if (button.innerText === '/') {
-      console.log('divide');
+      isDivideActive = true;
     }
 
     if (button.innerText === 'x') {
-      console.log('multiply');
+      isMultiplyActive = true;
     }
 
     if (button.innerText === '-') {
-      console.log('subtract');
+      isSubtractActive = true;
     }
 
     if (button.innerText === '+') {
-      console.log('add');
+      isAddActive = true;
     }
-
   });
 });
 
 
 clear.addEventListener('click', function() {
+
   display.innerText = 0;
-  decimal.removeAttribute('disabled');
+  total = 0;
+  enter.removeAttribute('disabled');
+  reset();
 });
 
 
 decimal.addEventListener('click', function() {
-  display.innerText = display.innerText + this.innerText;
   this.setAttribute('disabled', '');
+
+  display.innerText = display.innerText + this.innerText;
+  if (
+    isDivideActive === true || 
+    isMultiplyActive === true || 
+    isSubtractActive === true || 
+    isAddActive === true
+  ) secondNum = display.innerText;
+  else firstNum = display.innerText;
 });
 
 
@@ -70,9 +100,46 @@ backspace.addEventListener('click', function() {
   display.innerText = display.innerText.substring(0, display.innerText.length - 1);
 
   if (display.innerText.length === 0) display.innerText = '0';
+  if (
+    isDivideActive === true || 
+    isMultiplyActive === true || 
+    isSubtractActive === true || 
+    isAddActive === true
+  ) secondNum = display.innerText;
+  else firstNum = display.innerText;
 });
 
 
 enter.addEventListener('click', function() {
-  console.log('equals');
-})
+
+  doMath(firstNum, secondNum);
+  this.setAttribute('disabled', '');
+  reset();
+});
+
+// functions
+function manageDisplay(button) {
+  if (display.innerText === '0') display.innerText = '';
+
+  display.innerText = display.innerText + button.innerText;
+};
+
+function doMath(firstNum, secondNum) {
+  let first = parseFloat(firstNum);
+  let second = parseFloat(secondNum);
+
+  if (isDivideActive === true) total = first / second;
+  if (isMultiplyActive === true) total = first * second;
+  if (isSubtractActive === true) total = first - second;
+  if (isAddActive === true) total = first + second;
+
+  display.innerText = total;
+};
+
+function reset() {
+  decimal.removeAttribute('disabled');
+  isDivideActive = false;
+  isMultiplyActive = false;
+  isSubtractActive = false;
+  isAddActive = false;
+};
