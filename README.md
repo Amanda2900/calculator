40100fc6aa83fbe7158fd0d69cbb3b6c57407476
Amanda DiNoto <amanda.dinoto@gmail.com>
2024-04-04 21:49:29 -0400

    add basic non-math functionality to all buttons including color changes on click

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-This is a project for coding practice. 
+# Calculator
+
+## Description
+
+I built a calculator. This is a project for coding practice. 
+
+## Technology
+
+- JavaScript
+- CSS
+- HTML
 
 
-Credits
+## Credits
 
-Favicon: <a href="https://www.flaticon.com/free-icons/calculator" title="calculator icons">Calculator icons created by Pixel perfect - Flaticon</a>
+- Favicon: <a href="https://www.flaticon.com/free-icons/calculator" title="calculator icons">Calculator icons created by Pixel perfect - Flaticon</a>
--- js/app.js
+++ js/app.js
@@ -7,3 +7,68 @@
 
   - Button for light/dark mode
 */ 
+
+//Buttons
+const numButtons = document.querySelectorAll('button.num');
+const opButtons = document.querySelectorAll('button.operation');
+const clear = document.getElementById('clear');
+const backspace = document.getElementById('backspace');
+const decimal = document.getElementById('decimal');
+const enter = document.getElementById('enter');
+
+//Other elements
+const display = document.getElementById('num-display');
+
+
+numButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    if (display.innerText === '0') display.innerText = '';
+
+    display.innerText = display.innerText + button.innerText;
+  });
+});
+
+
+opButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+
+    if (button.innerText === '/') {
+      console.log('divide');
+    }
+
+    if (button.innerText === 'x') {
+      console.log('multiply');
+    }
+
+    if (button.innerText === '-') {
+      console.log('subtract');
+    }
+
+    if (button.innerText === '+') {
+      console.log('add');
+    }
+
+  });
+});
+
+
+clear.addEventListener('click', function() {
+  display.innerText = 0;
+});
+
+
+decimal.addEventListener('click', function() {
+  display.innerText = display.innerText + this.innerText;
+});
+
+
+backspace.addEventListener('click', function() {
+  display.innerText = display.innerText.substring(0, display.innerText.length - 1);
+
+  if (display.innerText.length === 0) display.innerText = '0';
+});
+
+
+enter.addEventListener('click', function() {
+  console.log('equals');
+})
